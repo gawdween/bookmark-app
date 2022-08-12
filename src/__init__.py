029b54b500f4b4ed6897afa8050eaa8621217396
Godwin E <Gawdween@gmail.com>
2022-08-12 11:30:28 +0100

    Added api  blueprints

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import os
+from src.auth import auth
+from src.bookmarks import bookmarks
 
 
 def create_app(test_config=None):
@@ -13,8 +15,6 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    @app.route('/')
-    def hello():
-        return 'Hello World!'
-
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
     return app
